407897bdf9293179cc4619eaf0f678f0a92b4654
Madison Carr <madison.carr@utexas.edu>
2020-07-15 08:21:10 -0500

    starts on route

--- src/main/java/com/example/todoapp/model/User.java
+++ src/main/java/com/example/todoapp/model/User.java
@@ -12,6 +12,7 @@ import java.util.Set;
 @Entity
 @RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "app_user")
 public class User {
 
---
+++ src/main/java/com/example/todoapp/web/UserController.java
@@ -0,0 +1,13 @@
+package com.example.todoapp.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/user")
+    public ResponseEntity<?> getUser()
+}
